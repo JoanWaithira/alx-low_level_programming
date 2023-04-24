bff3d0bd4881ef0fecc9a7b93b95047509aac6b1
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-24 04:38:08 -0700

    Using the ASCII chart to print lowercase and uppercase alphabet characters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase then uppercase
+ *
+ * Return:  Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+	int N = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (N <= 90)
+	{
+		putchar(N);
+		N++;
+	}
+	putchar('\n');
+
+	return (0);
+}
