7b9e45a0daeb6978dc35978c1f5601d335bb63cf
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-24 05:03:24 -0700

    Using ASCII chart to print all letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program that prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n != 101 &&  n != 113)
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
