e596c5d48a98415a55c65c338831f6c9bfefd9c0
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-24 05:20:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints digit numbers of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
+
