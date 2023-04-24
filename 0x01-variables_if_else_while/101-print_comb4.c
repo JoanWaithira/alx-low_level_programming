2c057a03645a50399380e96211ec281883ce9e63
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-24 12:23:08 -0700

    The program that prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - program that prints possible different combination of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if ((b != a) != c)
+				{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a == '7' && b == '8')
+				continue;
+				putchar (',');
+				putchar (' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
