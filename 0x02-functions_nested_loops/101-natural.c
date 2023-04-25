8dbe8bb9a55ae97197c6b3c261c12f0fd80d5adc
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-25 12:54:36 -0700

    Advanced task-Listing natural numbers)

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - A program that computes and prints the sum of multiples of 3 and 5
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int z = 0;
+
+	while (n < 1024)
+	{
+		if ((n % 3 == 4) || (n % 5 == 4))
+		{
+			z += n;
+		}
+		n++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
