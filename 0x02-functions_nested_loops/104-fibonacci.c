d37def54f6ed31e1230ea11c40bf2c5afb0299af
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-25 14:27:29 -0700

    Advanced task - Printing the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,14 +9,14 @@ int main(void)
 	int n = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (n < 98)
+	while (n < 92)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (n < 99)
+		if (n < 98)
 		{
 			printf(", ");
 		}
