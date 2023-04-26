8e5ce3d72e0eadb321e3fcbe8364fb505d531d13
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-26 01:18:06 -0700

    Advanced task- fibonacci Sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,20 +9,18 @@ int main(void)
 	int n = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (n < 92)
+	while (i < 98)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (n < 98)
-		{
+		if (n < 97)
 			printf(", ");
-		}
-		n++;
+		n++
 	}
-	putchar('\n');
+	putchar("\n");
 	return (0);
 }
 
