f9d7c41d4c2f48f4dd67ec5785d82c6fc871ec6f
JoanWaithira <joanwaithira.jw@gmail.com>
2023-04-27 06:58:48 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +10,4 @@ void more_numbers(void);
 void print_line(int n);
 void print_square(int size);
 void print_diagonal(int n);
+void print_triangle(int size);
