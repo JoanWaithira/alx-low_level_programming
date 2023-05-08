3539e0499150d59ac8a6e25bfcd37fb1a41b09df
JoanWaithira <joanwaithira.jw@gmail.com>
2023-05-08 03:30:10 -0700

    _memcpy - A function that cpoies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int a;
+	int b;
 
 	a = 0;
+	b = n;
 
-	for (a = 0; a < 0; a++)
+	for (a = 0; a < b; a++)
 	{
 		dest[a] = src[a];
 		n--;
