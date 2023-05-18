05a20de6615ee6de1fe789d898d83d4278daadf9
JoanWaithira <joanwaithira.jw@gmail.com>
2023-05-18 06:00:34 -0700

    _calloc - A function that allocates memory for an array

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,12 +12,8 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int a = 0, b = 0, c, d;
+	unsigned int a = 0, b = 0, c;
 	char *ptr;
-
-	for (; s1[a] != '\0'; a++)
-
-	for (; s2[b] != '\0'; b++)
 
 	if (s1 == NULL)
 	{
@@ -27,27 +23,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	for (a = 0; s1[a] != '\0'; a++)
+
+	for (b = 0; s2[b] != '\0'; b++)
 
 	if (n > b)
-	{
-		n = b;
-	}
-	c = a + n;
-	ptr = malloc(c + 1);
+	n = b;
+	ptr = malloc((a + n + 1) * sizeof(char));
 
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (d = 0; d < c; d++)
+	for (c = 0; c < a; c++)
 	{
-		if (d < a)
-		{
-			ptr[d] = s1[d];
-		}
-		else
-			ptr[d] = s2[d - a];
+		ptr[c] = s1[c];
 	}
-	ptr[d] = '\0';
+	for (; c < (a + n); c++)
+	{
+		ptr[c] = s2[c - a];
+	}
+	ptr[c] = '\0';
 	return (ptr);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,5 +10,23 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	return (ptr)
+	unsigned int a = 0;
+	char *ptr;
+	
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (; a < nmemb * size; a++)
+	{
+		ptr[a] = 0;
+	}
+
+	return (ptr);
 }
