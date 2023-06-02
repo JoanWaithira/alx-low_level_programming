edb12a1e1cfd83de9db524cad19a86334a3f5fbb
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-02 06:09:13 -0700

    A function that prints a sentence before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+void print(void);
+
+/**
+ * print - A function that prints a sentence before the main function executes
+ *
+ * Return: void
+ */
+void print (void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
