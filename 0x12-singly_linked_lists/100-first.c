981006944b58e8574e3a7d64a5c2748af5899bc7
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-04 23:36:53 -0700

    Corrections

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include "lists.h"
+#include <stdio.h>
 
-void print(void);
+void print(void) __attribute__ ((constructor));
 
 /**
  * print - A function that prints a sentence before the main function executes
  *
  * Return: void
  */
-void print (void)
+void print(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
