af55329f52c79d854a5fdc9259dab2d64e041205
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-06 06:11:35 -0700

    Debugging

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,12 +14,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *current = NULL;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return (-1);
 	}
 	if (index == 0)
 	{
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 		return (-1);
