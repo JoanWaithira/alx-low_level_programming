4c94a4542edbf05d2c5bb81a3a57359985957d17
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-07 01:54:25 -0700

    Debugging

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,3 +10,29 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *tortoise = head;
+	listint_t *hare = head;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (tortoise && hare && hare->next)
+	{
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+
+		if (hare == tortoise)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (hare);
+		}
+	}
+	return (NULL);
+}
