2f39881b5fe10ba0a1acd7363261f93dfa74bba4
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-07 01:16:29 -0700

    Debugging

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,32 +9,32 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t len = 0;
-    listint_t *current = *h;
-    listint_t *temp = NULL;
+	size_t len = 0;
+       	listint_t *current = *h;
+	listint_t *temp = NULL;
+	
+	if (!h || !*h)
+	{
+		return (len);
+	}
+	
+	while (current != NULL)
+	{
+		if (current <= current->next)
+		{
+			temp = current;
+			current = current->next;
+			free(temp);
+			len++;
+		}
+		else
+		{
+			free(current);
+			len++;
+			break;
+		}
+	}
 
-    if (!h || !*h)
-        return len;
-
-    while (current != NULL)
-    {
-        if (current <= current->next)
-        {
-            temp = current;
-            current = current->next;
-            free(temp);
-            len++;
-        }
-        else
-        {
-            free(current);
-            len++;
-            break;
-        }
-    }
-
-    *h = NULL;
-
-    return len;
+	*h = NULL;
+	return (len);
 }
-
