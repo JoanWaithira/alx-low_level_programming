fec2443be1b52d591ca604483840a8deaac421f0
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-08 12:22:22 -0700

    A function that counts bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,4 +10,22 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	int a = 63;
+	unsigned int count = 0;
+	unsigned long int qst;
+	unsigned long int result = n ^ m;
+
+	for (; a >= 0; a--)
+	{
+		qst = result >> a;
+
+		if (qst & 1)
+		{
+			count++;
+		}
+	}
+	return (count);
+}
+
+
 
