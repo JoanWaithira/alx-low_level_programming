f17420372441831e8220322ed79b15c2c9297844
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-12 05:31:49 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * create_file - A function that creates a file
+ *
+ * @filename: The name of file to create
+ *
+ * @text_context: a NULL terminated string to write a file
+ *
+ * Return: Always 0 (Success)
+ */
+int create_file(const char *filename, char *text_content)
+{
+
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - A function that appends text at the end of a file
+ *
+ * @filename: The name of the file
+ * @text_content: The NULL terminated string to add at the end of a file
+ *
+ * Return: 1 if Success -1 if it fails
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+}
