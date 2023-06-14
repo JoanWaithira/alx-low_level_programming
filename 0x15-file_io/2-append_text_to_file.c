257ea5f7757d1fc0ec7e8f631e3e528fa23859da
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-13 23:16:06 -0700

    Debugging

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int a;
 	int b;
-	int length = 0;
+	int length;
 
 	if (filename == NULL)
 	{
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (; text_content[length]; length++);
+		for (length = 0; text_content[length]; length++);
 	}
 	a = open(filename, O_WRONLY | O_APPEND);
 
