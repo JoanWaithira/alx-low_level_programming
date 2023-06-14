f677577c8f68fa59324f56f8f38078cb72fda3ce
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-13 23:27:53 -0700

    Debugging

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (; text_content[length]; length++);
+		for (; text_content[length];)
+		{
+			length++;
+		}
 
 	}
 	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
