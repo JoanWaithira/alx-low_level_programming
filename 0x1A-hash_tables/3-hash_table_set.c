ad1b519d0eb7ead888986abac06f477761404a4b
JoanWaithira <joanwaithira.jw@gmail.com>
2023-09-08 01:22:43 -0700

    Fixing task 0

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Write a function that adds an element to the hash table.
+ * ht: the hash table
+ * key: is key
+ * value: value associated with key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
