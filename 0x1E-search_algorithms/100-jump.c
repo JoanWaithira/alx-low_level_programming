8bdd92a7797de1c771ecdf448d4a3b025f4b2ce9
JoanWaithira <joanwaithira.jw@gmail.com>
2023-12-13 00:44:28 -0800

    Big O #5

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - A function taht searches for a value in a sorted array of integers
+ * @array: A pointer to the first element of array
+ * @size: The number of elements in array
+ * @value: The value to search for
+ * Return: the first index where value is located
+ */
+int jump_search(int *array, size_t size, int value)
+{
+
